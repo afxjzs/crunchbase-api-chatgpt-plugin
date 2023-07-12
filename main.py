d7b8e1540cc00f9a9b449d43bb374084755243f2
doug <afxjzs@users.noreply.github.com>
2023-07-12 18:34:19 +0000

    Update main.py to include full data

--- main.py
+++ main.py
@@ -8,8 +8,10 @@ import os
 
 app = FastAPI()
 
+
 class Company(BaseModel):
     name: str
+
 
 @app.post("/search")
 async def search_company(company: Company):
@@ -28,7 +30,7 @@ async def search_company(company: Company):
         "limit": 5
     }
     response = requests.post(url, data=json.dumps(data))
-    print("\n",data,"\n")
+    print("\n", data, "\n")
     print(response.json())
     response_data = response.json()
     if response.status_code == 200:
@@ -36,20 +38,36 @@ async def search_company(company: Company):
         extracted_data = []
         for entity in response_data['entities']:
             extracted_entity = {
-                'company_name': entity['properties'].get('name', None),
-                'description': entity['properties'].get('short_description', None),
-                'website_url': entity['properties'].get('website_url', None),
+                'created_at': entity['properties'].get('created_at', None),
+                'entity_def_id': entity['properties'].get('entity_def_id', None),
+                'facebook': entity['properties'].get('facebook', None),
+                'facet_ids': entity['properties'].get('facet_ids', None),
+                'identifier': entity['properties'].get('identifier', None),
+                'image_id': entity['properties'].get('image_id', None),
                 'image_url': entity['properties'].get('image_url', None),
+                'linkedin': entity['properties'].get('linkedin', None),
+                'location_identifiers': entity['properties'].get('location_identifiers', None),
+                'name': entity['properties'].get('name', None),
+                'permalink': entity['properties'].get('permalink', None),
+                'short_description': entity['properties'].get('short_description', None),
+                'stock_exchange_symbol': entity['properties'].get('stock_exchange_symbol', None),
+                'stock_symbol': entity['properties'].get('stock_symbol', None),
+                'twitter': entity['properties'].get('twitter', None),
+                'updated_at': entity['properties'].get('updated_at', None),
+                'uuid': entity['properties'].get('uuid', None),
+                'website': entity['properties'].get('website', None),
             }
             extracted_data.append(extracted_entity)
         # Convert the extracted data to a JSON string
         extracted_data_json = {'results': extracted_data}
         print("\n", extracted_data_json)
-        
+
         return extracted_data_json
         # return response.json()
     else:
-        raise HTTPException(status_code=400, detail="Unable to fetch data from Crunchbase API")
+        raise HTTPException(
+            status_code=400, detail="Unable to fetch data from Crunchbase API")
+
 
 @app.get("/.well-known/ai-plugin.json", include_in_schema=False)
 async def read_manifest():
@@ -59,6 +77,7 @@ async def read_manifest():
         return JSONResponse(content=data)
     except FileNotFoundError:
         raise HTTPException(status_code=404, detail="manifest.json not found")
+
 
 @app.get("/openai.json")
 async def get_openapi_json():
